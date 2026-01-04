f4db59ec584ec2e72ea951c35d1d00c403cb1de0
Kshitiz Bhandari <bhandari_kshitiz@yahoo.com>
2026-01-03 23:47:54 -0800

    notif and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,10 @@ import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json()); // to access req.body
 
 app.use(clerkMiddleware()); //handle authentication
+app.use(arcjetMiddleware);
 //connectDB();
 
 app.get("/", (req, res) => res.send("Hello from server!!!"));
@@ -20,6 +23,7 @@ app.get("/", (req, res) => res.send("Hello from server!!!"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res) => {
